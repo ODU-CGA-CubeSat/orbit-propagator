5b5a7ad77c5144cd739b68bd5b7cd2d36b249a4e
Ethan Senecal <esene002@odu.edu>
2022-07-13 13:14:42 -0400

    Resolved seg faults running VintiSimulationGNU.m

--- VintiSimulationGNU.m
+++ VintiSimulationGNU.m
@@ -1,8 +1,7 @@
 %% Vinti Simulation
 
-%clear
 format long g
-
+close all; clear all; delete('build/outputStateVect.txt')
 % need real mass
 SatMass = 2;                                % kg
 r_earth = 6.3781*10^3;                      %km
@@ -18,6 +17,7 @@ DragParam=(DragParam_tumbling);  % N (m^2/s^2)^-1
 % Actual orbial data
 %   determine time step needed (currently hard coded as 20 s)
 dt = 20; %s
+dragParamAltIncr = 0.01; %km
 
 n = 100 * 270; % X * 1.5 hrs (~1 orbit)
 x_ECI = nan(6,n);
@@ -34,28 +34,29 @@ FD_avg = nan(n,1);
 
 %x_ECI(:,1) = table2array(readtable("inputStateVect.txt"));
 
+cd build
 for i=1:n
 % Call C code Vinti Executable
-system('build/VintiCode')
+system('./VintiCode')
 
 %Get Data from Output of Vinti program
-VintiOutput = csvread("build/outputStateVect.txt");
+VintiOutput = csvread("outputStateVect.txt");
 %Store ECI state vector
 x_ECI(:,i) = VintiOutput(1:6);
 %Store Classical Mean elements State Vector
 x_meanElements(:,i) = VintiOutput(7:12); %needs updating
 
-altitude_loop(i) = round((norm([x_ECI(1,i) x_ECI(2,i) x_ECI(3,i)]) - r_earth)*100)/100;           %km
+altitude_loop(i) = (norm([x_ECI(1,i) x_ECI(2,i) x_ECI(3,i)]) - r_earth);           %km
 Veloc_loop(i,:) = [x_ECI(4,i) x_ECI(5,i) x_ECI(6,i)]*1000; V(i) = norm(Veloc_loop(i,:));    %m/s
 velocUnitVector(i,:) = Veloc_loop(i,:)./V(i);
-FD_avg(i) = DragParam(round((altitude_loop(i)-0)/.01+1),2) * V(i)^2;                         %N
+FD_avg(i) = DragParam(round((altitude_loop(i)-0)/dragParamAltIncr+1),2) * V(i)^2;                         %N
 
 dV(i) = (FD_avg(i)*dt/SatMass); V2(i) = V(i) - dV(i);                                %m/s         
 % convert this value back into state vector
 x_ECI(4:6,i) = V2(i)*velocUnitVector(i,:)/1000;                                      %km/s
 
 % Send new ECI State Vector to input file for use by Vinti C program
-csvwrite("build/inputStateVect.txt",x_ECI(:,i))
+csvwrite("inputStateVect.txt",x_ECI(:,i))
 end
 
 a = x_meanElements(1,:); e = x_meanElements(2,:);
@@ -66,10 +67,11 @@ plot3(x_ECI(1,n),x_ECI(2,n),x_ECI(3,n),'*k')
 xlabel('x (km)')
 ylabel('y (km)')
 zlabel('z (km)')
-[x,y,z] = sphere;
-x = x*r_earth; y = y*r_earth; z = z*r_earth;
-surf(x,y,z), shading interp
-legend('orbit','start','end','earth')
+##[x,y,z] = sphere;
+##x = x*r_earth; y = y*r_earth; z = z*r_earth;
+##surf(x,y,z), shading interp
+legend('orbit','start','end')%,'earth')
 grid on, axis square
 xlim([-10000 10000]),ylim([-10000 10000]),zlim([-10000 10000])
 hold off
+cd ..
