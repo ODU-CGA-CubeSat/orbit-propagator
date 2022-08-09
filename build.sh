385f2d182d3ac46ee5b995c805cdc108c17a0d5f
Sean Marquez <capsulecorplab@gmail.com>
2022-08-09 15:57:35 -0600

    ls root & build directory for debugging

--- build.sh
+++ build.sh
@@ -12,7 +12,8 @@ fi
 if ! $clitool --version | grep $version > /dev/null; then
     echo "$clitool $version not installed"
     echo "running build via docker..."
-    docker run --rm -v "$PWD:/src" -w "/src" $dockerimage bash -c 'cd build && $cmd && make && ctest -V'
+#    docker run --rm -v "$PWD:/src" -w "/src" $dockerimage bash -c 'cd build && $cmd && make && ctest -V'
+    docker run --rm -v "$PWD:/src" -w "/src" $dockerimage bash -c 'ls && cd build && ls'
 else
     echo "$clitool $version installed!"
     echo "running build..."
