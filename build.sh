020f7a980cd9912afb6f1c6b686a87893b445c8c
Sean Marquez <capsulecorplab@gmail.com>
2022-08-15 17:52:34 -0600

    don't use bash alias for running cmake in docker

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 clitool="cmake"
 version=3.23.1
-cmd="$clitool .."
 dockerimage="rikorose/gcc-cmake:gcc-9"
 
 if [ ! -r ./build ]; then
@@ -12,13 +11,12 @@ fi
 if ! $clitool --version | grep $version > /dev/null; then
     echo "$clitool $version not installed"
     echo "running build via docker..."
-#    docker run --rm -v "$PWD:/src" -w "/src" $dockerimage bash -c 'cd build && $cmd && make && ctest -V'
-    docker run --rm -v "$PWD:/src" -w "/src" $dockerimage bash -c 'ls && cd build && ls'
+    docker run --rm -v "$PWD:/src" -w "/src" $dockerimage bash -c 'cd build && cmake .. && make && ctest -V'
 else
     echo "$clitool $version installed!"
     echo "running build..."
     cd build
-    $cmd
+    cmake ..
     make
     ctest -V
 fi
