667391455b8e8ff2eb841518ff83a812238d8b01
Ethan Senecal <esene002@odu.edu>
2022-08-30 12:02:53 -0400

    Update cmake build system with debug symbols

--- build.sh
+++ build.sh
@@ -11,12 +11,12 @@ fi
 if ! $clitool --version | grep $version > /dev/null; then
     echo "$clitool $version not installed"
     echo "running build via docker..."
-    docker run --rm -v "$PWD:/src" -w "/src" $dockerimage bash -c 'cd build && cmake .. && make && ctest -V'
+    docker run --rm -v "$PWD:/src" -w "/src" $dockerimage bash -c 'cd build && cmake .. -DCMAKE_BUILD_TYPE=Debug && make && ctest -V'
 else
     echo "$clitool $version installed!"
     echo "running build..."
     cd build
-    cmake ..
+    cmake .. -DCMAKE_BUILD_TYPE=Debug
     make
     ctest -V
 fi
